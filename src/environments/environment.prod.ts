f77ab1ab9abb5b519e8b63216fa409da93cee0e9
juva <juvet.quentin19@gmail.com>
2023-07-08 16:58:06 +0200

    try to fix gitlab ci with patch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDRNiUSrIhGYOZkvL7Y2KgU_gHgEqnxcwI",
+    authDomain: "athleteiq-2f163.firebaseapp.com",
+    projectId: "athleteiq-2f163",
+    storageBucket: "athleteiq-2f163.appspot.com",
+    messagingSenderId: "246652916677",
+    appId: "1:246652916677:web:3a61e871da72e24be331ac",
+    measurementId: "G-RYYS350MB1"
+  }
 };
